89d471e23015c64d9e9ab559fe94e6cc1bfe113c
browning <bwbrowning@gmail.com>
2014-09-14 20:16:36 -0500

    create a basic goroutine and channel for spidering and passing back the status

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -3,6 +3,13 @@ package controllers
 import "github.com/revel/revel"
 import "fmt"
 
+var messages = make(chan string)
+
+func spider(url string) {
+    fmt.Printf("running spider func in a goroutine")
+    messages <- "doing go stuff"
+}
+
 type App struct {
 	*revel.Controller
 }
@@ -13,5 +20,11 @@ func (c App) Index() revel.Result {
 
 func (c App) StartSpider(url string) revel.Result {
 	fmt.Printf("%s\n", url)
+	go spider(url)
 	return c.Render()
+}
+
+func (c App) SpiderStatus() revel.Result {
+	status := <- messages
+	return c.RenderText(status)
 }
