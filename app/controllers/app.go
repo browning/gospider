c16196a77f2aa5f243a4c36ecf76eac35f4bb238
browning <bwbrowning@gmail.com>
2014-09-14 21:09:47 -0500

    scrape all the links on a page

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -2,12 +2,37 @@ package controllers
 
 import "github.com/revel/revel"
 import "fmt"
+import "net/http"
+import "code.google.com/p/go.net/html"
 
 var messages = make(chan string)
+
+func parse_html(n *html.Node) {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, element := range n.Attr {
+			if element.Key == "href" {
+				fmt.Printf("LINK: %s\n", element.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		parse_html(c)
+	}
+}
 
 func spider(url string) {
     fmt.Printf("running spider func in a goroutine")
     messages <- "doing go stuff"
+
+    response, err := http.Get(url)
+    if err != nil {
+        fmt.Printf("%s", err)
+    } else {
+    	doc, err := html.Parse(response.Body)
+    	fmt.Printf("%s", err)
+    	parse_html(doc)
+    }
+
 }
 
 type App struct {
