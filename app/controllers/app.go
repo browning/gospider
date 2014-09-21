244ca5109c7c873840ed65b9856c0efa91349378
browning <bwbrowning@gmail.com>
2014-09-20 21:49:01 -0500

    finished spidering code

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -3,15 +3,20 @@ package controllers
 import "github.com/revel/revel"
 import "fmt"
 import "net/http"
+import urlhelpers "net/url"
 import "code.google.com/p/go.net/html"
 
 var messages = make(chan string)
+var basehostname string
+var scrape_results = make(map[string]string)
 
 func parse_html(n *html.Node) {
 	if n.Type == html.ElementNode && n.Data == "a" {
 		for _, element := range n.Attr {
 			if element.Key == "href" {
 				fmt.Printf("LINK: %s\n", element.Val)
+
+				go spider(element.Val)
 			}
 		}
 	}
@@ -22,14 +27,30 @@ func parse_html(n *html.Node) {
 
 func spider(url string) {
     fmt.Printf("running spider func in a goroutine")
-    messages <- "doing go stuff"
+    
+
+    _, present := scrape_results[url]
+    fmt.Printf("\npresent %r\n", present)
+    if (present) {
+    	return
+    } else {
+    	scrape_results[url] = ""
+    }
+
+    messages <- "spidering " + url + "<br />"
+
+    url_host, _ := urlhelpers.Parse(url)
+    if (url_host.Host != basehostname) {
+    	return
+    }
 
     response, err := http.Get(url)
     if err != nil {
         fmt.Printf("%s", err)
     } else {
-    	doc, err := html.Parse(response.Body)
-    	fmt.Printf("%s", err)
+    	doc, _ := html.Parse(response.Body)
+    	//scrape_results[url] = doc.Data
+    	fmt.Printf("%s", doc)
     	parse_html(doc)
     }
 
@@ -45,6 +66,8 @@ func (c App) Index() revel.Result {
 
 func (c App) StartSpider(url string) revel.Result {
 	fmt.Printf("%s\n", url)
+	parsed_url, _ := urlhelpers.Parse(url)
+	basehostname = parsed_url.Host
 	go spider(url)
 	return c.Render()
 }
